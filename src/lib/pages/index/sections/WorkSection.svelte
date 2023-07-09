3934b84eb80db88c23ce5c6396fd4840443cad64
Georgiy Vlasov <wlasowegor@gmail.com>
2023-07-10 02:58:03 +0400

    Back up

--- src/lib/pages/index/sections/WorkSection.svelte
+++ src/lib/pages/index/sections/WorkSection.svelte
@@ -51,7 +51,6 @@
   @import "../../../../variables";
 
   section.work {
-    margin-top: 244px;
     display: grid;
     grid-template-columns: auto 16.85% 16.99% 32.53%;
     grid-template-rows: repeat(6, min-content);
@@ -63,17 +62,18 @@
       box-shadow: 0 0 20px rgb(93 93 93 / 24%);
     }
 
+    margin-top: 14vw;
+
     .section-header {
       grid-column: 1/3;
       grid-row: 1;
-      margin-top: -8px;
-      margin-left: 2px;
+      margin-left: -0rem;
     }
 
     .photo1 {
       grid-column: 1;
       grid-row: 2;
-      margin-top: 29.0%;
+      margin-top: 65.0%;
       margin-right: 2.2%;
       margin-left: 1.3%;
     }
@@ -81,7 +81,7 @@
     .photo2 {
       grid-column: 1;
       grid-row: 3;
-      margin-top: 6.3%;
+      margin-top: 5.3%;
       border-right: .38vw solid transparent;
       margin-left: 1.3%;
     }
@@ -89,7 +89,7 @@
     .photo3 {
       grid-column: 2;
       grid-row: 2/4;
-      margin-top: 10.3%;
+      margin-top: 50.1%;
       margin-left: -2.7%;
       margin-right: 2.6%;
     }
@@ -97,183 +97,57 @@
     .photo4 {
       grid-column: 3;
       grid-row: 2/4;
-      margin-top: -21.9%;
+      margin-top: 16.9%;
     }
 
     .photo5 {
       grid-column: 4;
       grid-row: 1/5;
-      margin-top: -8.5%;
+      margin-top: -3.1%;
     }
 
     .photo6 {
       grid-column: 1/4;
       grid-row: 4/6;
-      margin-top: 4.4%;
-      margin-right: 6.9%;
+      margin-top: 4.9%;
+      margin-right: 7%;
     }
 
     .photo7 {
       grid-column: 4;
       grid-row: 5;
-      margin-left: -8.4%;
+      margin-left: -13.4%;
 
       img {
-        transform: translateY(22.2%);
+        transform: translateY(15.2%);
       }
     }
 
     .photo8 {
       grid-column: 1/3;
       grid-row: 6;
-      margin-top: 14.8%;
-      margin-left: 13%;
-      margin-right: 0.5%;
+      margin-top: 8.2%;
+      margin-left: 12.9%;
+      margin-right: -3%;
     }
 
     .photo9 {
       grid-column: 3/5;
       grid-row: 6;
-      margin-top: 23.8%;
-      margin-left: 5%;
+      margin-top: 14.6%;
+      margin-left: 2.5%;
     }
 
     .photo10 {
       grid-column: 1/5;
       grid-row: 7;
-      margin-top: 7.8%;
+      margin-top: 3.4%;
       margin-left: 8.26%;
-      margin-right: 23.3%;
+      margin-right: 26.8%;
     }
 
-    @media screen and (max-width: 1680px) {
-      margin-top: 214px;
-      .section-header {
-        $shift: 7px;
-        margin-top: 0-$shift;
-        padding-bottom: $shift;
-        margin-left: 0px;
-      }
-      .photo5 {
-        margin-top: -8.3%;
-      }
-      .photo4 {
-        margin-top: -25.7%;
-      }
-      .photo3 {
-        margin-top: 7.3%;
-      }
-      .photo2 {
-        margin-top: 3.3%;
-        border-right: .38vw solid transparent;
-      }
-      .photo1 {
-        margin-top: 26.0%;
-        margin-right: 2.2%;
-        margin-left: 1.3%;
-      }
-      .photo6 {
-        margin-top: 4.4%;
-      }
-
-      .photo8 {
-        margin-top: 14.3%;
-      }
-
-      .photo9 {
-        margin-top: 23.5%;
-        margin-left: 5%;
-      }
-
-      .photo10 {
-        margin-top: 7.7%;
-      }
-    }
-
-    @media screen and (max-width: 639px) {
-      margin-top: 41px;
-
-      .section-header {
-        margin-left: -1px;
-      }
-
-      .photo1 {
-        grid-column: 1;
-        grid-row: 2;
-        margin-top: 42.0%;
-        margin-right: 2.2%;
-        margin-left: 1.3%;
-      }
-
-      .photo2 {
-        grid-column: 1;
-        grid-row: 3;
-        margin-top: -3.3%;
-        border-right: .38vw solid transparent;
-        margin-left: 1.3%;
-      }
-
-      .photo3 {
-        grid-column: 2;
-        grid-row: 2/4;
-        margin-top: 24.1%;
-        margin-left: -2.7%;
-        margin-right: 2.6%;
-      }
-
-      .photo4 {
-        grid-column: 3;
-        grid-row: 2/4;
-        margin-top: -6.9%;
-      }
-
-      .photo5 {
-        grid-column: 4;
-        grid-row: 1/5;
-        margin-top: 3.1%;
-      }
-
-      .photo6 {
-        grid-column: 1/4;
-        grid-row: 4/6;
-        margin-top: 2.4%;
-        margin-right: 7%;
-      }
-
-      .photo7 {
-        grid-column: 4;
-        grid-row: 5;
-        margin-left: -13.4%;
-
-        img {
-          transform: translateY(15.2%);
-        }
-      }
-
-      .photo8 {
-        grid-column: 1/3;
-        grid-row: 6;
-        margin-top: 3.8%;
-        margin-left: 12%;
-        margin-right: -3%;
-      }
-
-      .photo9 {
-        grid-column: 3/5;
-        grid-row: 6;
-        margin-top: 11.8%;
-        margin-left: 2.5%;
-      }
-
-      .photo10 {
-        grid-column: 1/5;
-        grid-row: 7;
-        margin-top: 1.5%;
-        margin-left: 8.26%;
-        margin-right: 26.5%;
-      }
-
-    }
   }
+
+
 
 </style>
