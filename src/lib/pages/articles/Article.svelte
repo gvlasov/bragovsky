1b1386b12fc5da12d0ce17c3401a1e632a3760aa
Georgiy Vlasov <wlasowegor@gmail.com>
2023-07-12 03:43:10 +0400

    Back up

---
+++ src/lib/pages/articles/Article.svelte
@@ -0,0 +1,68 @@
+<script lang="ts">
+    import ShadowedContainer from "$lib/ShadowedContainer.svelte";
+</script>
+
+<ShadowedContainer>
+    <article>
+        <div class="description">
+            <h1>
+                <slot name="h1"/>
+            </h1>
+            <h2>
+                <slot name="h2"/>
+            </h2>
+            <h3>
+                <slot name="author"/>
+            </h3>
+        </div>
+        <slot name="content"/>
+    </article>
+</ShadowedContainer>
+
+<style lang="scss">
+  article {
+    font-family: Bruta Glb Regular, sans-serif;
+    padding: 4.15em 4.1em 4.15em 4.1em;
+    font-size: 1.012vw;
+
+    .description {
+      float: left;
+      padding-left: 3.5em;
+      padding-right: 3.6em;
+
+      :global(h1) {
+        font-size: 0.952vw;
+        line-height: 1.547em;
+        letter-spacing: 0.010em;
+        margin-top: 4.5em;
+        margin-left: 2.1em;
+      }
+
+      :global(h2) {
+        font-size: 1.488vw;
+        line-height: 1.91em;
+        letter-spacing: 0.023em;
+        max-width: 10em;
+        margin-top: 7em;
+        margin-left: 1.35em;
+      }
+
+      :global(h3) {
+        font-size: 1.488vw;
+        font-weight: 300;
+        line-height: 0.8em;
+        letter-spacing: 0.026em;
+        margin-top: 5.87em;
+        margin-left: 1.3em;
+        margin-bottom: 12em;
+      }
+    }
+
+    :global(p) {
+      font-size: 1.012vw;
+      line-height: 2.553em;
+      letter-spacing: 0.035em;
+      margin: 0 0 1.8em 0;
+    }
+  }
+</style>
--- src/routes/articles/search-truth/+page.svelte
+++ src/routes/articles/search-truth/+page.svelte
@@ -1,21 +1,19 @@
 <script lang="ts">
     import {_} from 'svelte-i18n'
-    import ShadowedContainer from "$lib/ShadowedContainer.svelte";
     import pinkVolkhov from '$assets/Сайт_optimal/Статьи/Этюд. Розовый волхов 6.jpg'
     import blueVolkhov from '$assets/Сайт_optimal/Живопись/_s-2901-2. Resolution 65.jpg'
     import petyaFruit from '$assets/Сайт_optimal/Статьи/Оригинал/Петя и фрукты 1970 (4).jpg'
     import art1 from '$assets/Сайт_optimal/Статьи/Творчество 1.jpg'
     import art2 from '$assets/Сайт_optimal/Статьи/Оригинал/Творчество 4.2.jpg'
+    import Article from "../../../lib/pages/articles/Article.svelte";
 </script>
 
 <div class="content">
-    <ShadowedContainer>
-        <article>
-            <div class="description">
-                <h1>{$_('page.life_truth.heading1')}</h1>
-                <h2>{$_('page.life_truth.heading2')}</h2>
-                <h3>{$_('page.life_truth.author')}</h3>
-            </div>
+    <Article>
+        <svelte:fragment slot="h1">{$_('page.life_truth.heading1')}</svelte:fragment>
+        <svelte:fragment slot="h2">{$_('page.life_truth.heading2')}</svelte:fragment>
+        <svelte:fragment slot="author">{$_('page.life_truth.author')}</svelte:fragment>
+        <svelte:fragment slot="content">
             <div class="part part1">
                 {@html $_('page.life_truth.text.part1')}
             </div>
@@ -44,8 +42,8 @@
                 <img src="{art1}"/>
                 <img src="{art2}"/>
             </div>
-        </article>
-    </ShadowedContainer>
+        </svelte:fragment>
+    </Article>
 </div>
 
 
@@ -54,49 +52,6 @@
     padding: 7vw 1.7vw;
 
     article {
-      font-family: Bruta Glb Regular, sans-serif;
-      padding: 4.15em 4.1em 4.15em 4.1em;
-      font-size: 1.012vw;
-
-      .description {
-        float: left;
-        padding-left: 3.5em;
-        padding-right: 3.6em;
-
-        :global(h1) {
-          font-size: 0.952vw;
-          line-height: 1.547em;
-          letter-spacing: 0.010em;
-          margin-top: 4.5em;
-          margin-left: 2.1em;
-        }
-
-        :global(h2) {
-          font-size: 1.488vw;
-          line-height: 1.91em;
-          letter-spacing: 0.023em;
-          max-width: 10em;
-          margin-top: 7em;
-          margin-left: 1.35em;
-        }
-
-        :global(h3) {
-          font-size: 1.488vw;
-          font-weight: 300;
-          line-height: 0.8em;
-          letter-spacing: 0.026em;
-          margin-top: 5.87em;
-          margin-left: 1.3em;
-          margin-bottom: 12em;
-        }
-      }
-
-      :global(p) {
-        font-size: 1.012vw;
-        line-height: 2.553em;
-        letter-spacing: 0.035em;
-        margin: 0 0 1.8em 0;
-      }
 
       .part1 {
         margin-bottom: 2.3em;
@@ -142,7 +97,6 @@
       }
 
       .part4 {
-
         padding-top: 0.5em;
 
         figure {
@@ -166,15 +120,17 @@
         margin-top: 16.3em;
         margin-bottom: 3em;
         display: flex;
-        justify-content: space-around;
+        justify-content: start;
+        gap: 6.3em;
 
         img:nth-child(1) {
           width: 20.6vw;
           height: auto;
+          margin-left: 3.2em;
         }
 
         img:nth-child(2) {
-          width: 41vw;
+          width: 41.4vw;
           height: auto;
         }
       }
