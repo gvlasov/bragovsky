d0831569fa9b5d16d29abe86b76342f81224faae
Georgiy Vlasov <wlasowegor@gmail.com>
2023-07-14 16:50:23 +0400

    Updates

---
+++ _app/immutable/chunks/Творчество 1.7f231dff.js
@@ -0,0 +1,1 @@
+const e=""+new URL("../assets/Творчество 1.7ea9ec38.jpg",import.meta.url).href;export{e as a};
---
+++ _app/immutable/chunks/Юный Художник 1.a68315de.js
@@ -0,0 +1,1 @@
+const e=""+new URL("../assets/Каталог 3.e3d83e5f.jpg",import.meta.url).href,t=""+new URL("../assets/Юный Художник 1.99db2b83.jpg",import.meta.url).href;export{e as a,t as b};
