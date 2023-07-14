696b67c461f5760928c626b18f877bc4be8396d7
Georgiy Vlasov <wlasowegor@gmail.com>
2023-07-14 16:46:31 +0400

    Back up

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,13 +2,13 @@
     import LifeSection from "../lib/pages/index/sections/LifeSection.svelte";
     import IntroSection from "../lib/pages/index/sections/IntroSection.svelte";
     import WorkSection from "../lib/pages/index/sections/WorkSection.svelte";
-    import Footer from "$lib/Footer.svelte";
+    import LegalRights from "$lib/LegalRights.svelte";
 </script>
 
 <IntroSection/>
 <LifeSection/>
 <WorkSection/>
-<Footer/>
+<LegalRights/>
 
 <style lang="scss">
 </style>
